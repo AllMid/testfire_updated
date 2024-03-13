2250fc089be38776cfeaaf46cd0e7917f509a6ce
AllMid <dimagre15@gmail.com>
2024-03-13 19:26:27 +0300

    change BaseTest -> BasePage

--- framework/BasePage.py
+++ framework/BasePage.py
@@ -6,8 +6,7 @@ class BasePage:
 
     #initialization of the web driver and the URL of the web resource
     def __init__(self, driver):
-        self.driver = driver
-        self.base_url = "http://testfire.net/login.jsp"                     
+        self.driver = driver                    
 
     #search for a specific element of a web resource by locator
     def find_element(self, locator,time=10):
@@ -18,16 +17,12 @@ class BasePage:
         return WebDriverWait(self.driver,time).until(EC.presence_of_all_elements_located(locator), message=f"Can't find elements by locator {locator}")
 
     #switching to an initialized URL using an initialized web driver
-    def go_to_site(self):
-        return self.driver.get(self.base_url)
+    def go_to_site(self, base_url):
+        return self.driver.get(base_url)
     
     #creates a string alert_txt, where it is indicated that alert_no (missing), 
     #then a search is performed and, if alert is found, the string changes to allert_yes
-    def get_alert_box(self):
-        alert_txt = "alert_no"
-        try:
-            WebDriverWait(self.driver, 3).until(EC.alert_is_present(), 'Timed out waiting for confirmation popup to appear')
-            alert_txt = "alert_yes"
-        finally:
-            return alert_txt
+    def find_element_alert(self):
+            return WebDriverWait(self.driver, 3).until(EC.alert_is_present(), 'Timed out waiting for confirmation popup to appear')
+
         
