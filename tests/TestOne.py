17fb52a55f8fc0336098e3756264bd51a27cf47b
AllMid <dimagre15@gmail.com>
2024-03-13 19:27:18 +0300

    change tests

--- tests/TestOne.py
+++ tests/TestOne.py
@@ -1,29 +1,30 @@
-from Pages.GooglePages import SearchHelper
+from tests.LoginPages import Google_login_page
+from tests.MainPage import Google_main_page
 
 #the test goes to the site, enters a username in the username field, enters an sql injection in the password field,
 #clicks the Login button and checks whether the transition to the user's web page has occurred or not
 def test_google_sql_injections_passwd(browser):
-    google_auth_page = SearchHelper(browser)
-    google_auth_page.go_to_site()
+    google_auth_page = Google_login_page(browser)
+    google_auth_page.go_to_site(Google_login_page.base_url)
     google_auth_page.enter_word_username("admin")
     google_auth_page.enter_word_password("' or 1=1--+")
     google_auth_page.click_on_the_login_button()
-    assert browser.current_url == "https://testfire.net/login.jsp"
+    assert not Google_login_page.find_element(Google_main_page.LOCATOR_GOOGLE_TEXT_CONGRATULATIONS)
 
 #the test goes to the site, enters an sql injection in the username field, enters a password in the password field,
 #clicks the Login button and checks whether the transition to the user's web page has occurred or not
 def test_google_sql_injections_uid(browser):
-    google_auth_page = SearchHelper(browser)
-    google_auth_page.go_to_site()
+    google_auth_page = Google_login_page(browser)
+    google_auth_page.go_to_site(Google_login_page.base_url)
     google_auth_page.enter_word_username("admin' or 1=1--+")
     google_auth_page.enter_word_password("passwd")
     google_auth_page.click_on_the_login_button()
-    assert browser.current_url == "https://testfire.net/login.jsp"
+    assert not Google_login_page.find_element(Google_main_page.LOCATOR_GOOGLE_TEXT_CONGRATULATIONS)
 
 #the test goes to the site, enters an sql injection in the search bar, clicks on the Go button and checks the address of the web page
 def test_google_sql_injections_search(browser):
-    google_auth_page = SearchHelper(browser)
-    google_auth_page.go_to_site()
+    google_auth_page = Google_login_page(browser)
+    google_auth_page.go_to_site(Google_login_page.base_url)
     google_auth_page.enter_word_search("' or 1=1--+")
     google_auth_page.click_on_the_search_button()
-    assert browser.current_url == "https://testfire.net/search.jsp?query=%27+or+1%3D1--%2B"
+    assert not Google_login_page.find_element(Google_main_page.LOCATOR_GOOGLE_TEXT_CONGRATULATIONS)
--- tests/TestTwo.py
+++ tests/TestTwo.py
@@ -1,30 +1,30 @@
-from Pages.GooglePages import SearchHelper
+from tests.LoginPages import Google_login_page
 
 #the test goes to the site, enters a username in the username field, enters an xss injection in the password field,
 #clicks the Login button and checks if there is an alert or not
 def test_google_xss_injections_passwd(browser):
-    google_auth_page = SearchHelper(browser)
+    google_auth_page = Google_login_page(browser)
     google_auth_page.go_to_site()
     google_auth_page.enter_word_username("admin")
     google_auth_page.enter_word_password("<script>alert(123)</script>")
     google_auth_page.click_on_the_login_button()
-    assert google_auth_page.get_alert_box() == "alert_no"
+    assert not google_auth_page.find_element_alert()
 
 #the test goes to the site, enters an xss injection in the username field, enters a password in the password field,
 #clicks the Login button and checks if there is an alert or not
 def test_google_xss_injections_uid(browser):
-    google_auth_page = SearchHelper(browser)
+    google_auth_page = Google_login_page(browser)
     google_auth_page.go_to_site()
     google_auth_page.enter_word_username("<script>alert(123)</script>")
     google_auth_page.enter_word_password("passwd")
     google_auth_page.click_on_the_login_button()
-    assert google_auth_page.get_alert_box() == "alert_no"
+    assert not google_auth_page.find_element_alert()
 
 #the test goes to the site, enters an xss injection in the search bar, 
 #clicks on the Go button and checks if there is an alert or not
 def test_google_xss_injections_search(browser):
-    google_auth_page = SearchHelper(browser)
+    google_auth_page = Google_login_page(browser)
     google_auth_page.go_to_site()
     google_auth_page.enter_word_search("<script>alert(123)</script>")
     google_auth_page.click_on_the_search_button()
-    assert google_auth_page.get_alert_box() == "alert_no"
+    assert not google_auth_page.find_element_alert()
