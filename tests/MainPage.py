6f627114b4a907286b38b6bac44949f8a911f9f5
AllMid <dimagre15@gmail.com>
2024-03-19 19:50:46 +0300

    0

--- tests/MainPage.py
+++
@@ -1,6 +0,0 @@
-from framework.BasePage import BasePage
-from selenium.webdriver.common.by import By
-
-class Google_main_page(BasePage): 
-    #localization of the search bar by id in the code of a web resource
-    LOCATOR_GOOGLE_TEXT_CONGRATULATIONS = (By.XPATH, "//*[@id='_ctl0__ctl0_Content_Main_promo']/table/tbody/tr[1]/td/h2")
